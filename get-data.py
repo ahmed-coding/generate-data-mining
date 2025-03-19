e2792109603247cdd7ba867bf78922154efd7579
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2025-03-19 23:46:42 +0300

    fix backetest

--- get-data.py
+++ get-data.py
@@ -11,65 +11,16 @@ import os
 
 symbols = [
     'BTC/USDT',
-    'ETH/USDT',
-    'BNB/USDT',
-    'SOL/USDT',
-    'SUI/USDT',
-    'ADA/USDT',
-    'XRP/USDT',
-    'DOT/USDT',
-    'LINK/USDT',
-    'LTC/USDT',
-    'BCH/USDT',
-    'XLM/USDT',
-    'AVAX/USDT',
-    'UNI/USDT',
-    'AAVE/USDT',
-    'XTZ/USDT',
-    'ATOM/USDT',
-    'VET/USDT',
-    'ALGO/USDT',
-    'HBAR/USDT',
-    'MKR/USDT',
-    'SNX/USDT',
-    'YFI/USDT',
-    'COMP/USDT',
-    'FTM/USDT',
-    'LUNA/USDT',
-    'MATIC/USDT',
-    'FIL/USDT',
-    'CHZ/USDT',
-    'THETA/USDT',
-    'LRC/USDT',
-    'ONE/USDT',
-    'SHIB/USDT',
-    'QNT/USDT',
-    'ENJ/USDT',
-    'FLOW/USDT',
-    'CAKE/USDT',
-    'CELO/USDT',
-    'BAT/USDT',
-    'REN/USDT',
-    'LDO/USDT',
-    'RNDR/USDT',
-    'XDC/USDT',
-    'SUSHI/USDT',
-    'BNT/USDT',
-    'ZEC/USDT',
-    'DCR/USDT',
-    'DGB/USDT',
-    'NEXO/USDT',
-    'RVN/USDT',
-    'KSM/USDT',
+    # 'ETH/USDT',
 ]
 
 intervals = [
-    '1h',
+    # '1h',
     # '30m',
     # '15m',
-    # '5m',
-    # '3m',
-    # '1m'
+    '5m',
+    '3m',
+    '1m'
 ]
 
 
@@ -180,24 +131,15 @@ def calculate_all_indicators(df):
 
     return df
 
-# Strategy: SMA Crossover for Long and Short Trades
-# def sma_crossover_strategy(df):
-#     df['signal'] = 0
-#     df['signal'][20:] = np.where(
-#         df['sma_20'][20:] > df['sma_50'][20:], 1, -1  # 1 for Buy, -1 for Sell
-#     )
-#     df['positions'] = df['signal'].diff()
-#     return df
-
 
 # Strategy: EMA_2 and EMA_3 Crossover Strategy
 
 def ema_crossover_strategy(df):
     df['signal'] = 0
-    # Buy signal when SMA_2 > SMA_3
-    df['signal'][df['ema_3'] > df['ema_5']] = 1
-    df['signal'][df['ema_3'] < df['ema_5']] = - \
-        1  # Sell signal when SMA_2 < SMA_3
+    # Buy signal when EMA_2 > EMA_5
+    df.loc[df['ema_3'] > df['ema_5'], 'signal'] = 1  # Buy signal
+    df.loc[df['ema_3'] < df['ema_5'], 'signal'] = - \
+        1  # Sell signal when EMA_2 < EMA_5
     df['positions'] = df['signal'].diff()
     return df
 
@@ -207,8 +149,8 @@ def ema_crossover_strategy(df):
 def sma_crossover_strategy(df):
     df['signal'] = 0
     # Buy signal when SMA_2 > SMA_3
-    df['signal'][df['sma_2'] > df['sma_3']] = 1
-    df['signal'][df['sma_2'] < df['sma_3']] = - \
+    df.loc[df['sma_2'] > df['sma_3'], 'signal'] = 1
+    df.loc[df['sma_2'] < df['sma_3'], 'signal'] = - \
         1  # Sell signal when SMA_2 < SMA_3
     df['positions'] = df['signal'].diff()
     return df
@@ -219,8 +161,8 @@ def sma_crossover_strategy(df):
 def rsi_strategy(df, rsi_buy_threshold=30, rsi_sell_threshold=70):
     df['signal'] = 0
     # Buy signal when RSI < 30 (oversold)
-    df['signal'][df['rsi'] < rsi_buy_threshold] = 1
-    df['signal'][df['rsi'] > rsi_sell_threshold] = - \
+    df.loc[df['rsi'] < rsi_buy_threshold, 'signal'] = 1
+    df.loc[df['rsi'] > rsi_sell_threshold, 'signal'] = - \
         1  # Sell signal when RSI > 70 (overbought)
     df['positions'] = df['signal'].diff()
     return df
@@ -235,7 +177,7 @@ def backtest_strategy(df, symbol, interval, tp_percent=5, sl_percent=1):
     entry_price = 0.0
 
     for index, row in df.iterrows():
-        if row['positions'] == 1 and not in_position:  # Buy signal (long)
+        if row['signal'] == 1 and not in_position:  # Buy signal (long)
             in_position = True
             position_type = 'long'
             entry_price = row['close']
@@ -252,7 +194,7 @@ def backtest_strategy(df, symbol, interval, tp_percent=5, sl_percent=1):
                 **{col: row[col] for col in df.columns if col not in ['signal', 'positions']}
             }
             trades.append(trade)
-        elif row['positions'] == -1 and not in_position:  # Sell signal (short)
+        elif row['signal'] == -1 and not in_position:  # Sell signal (short)
             in_position = True
             position_type = 'short'
             entry_price = row['close']
@@ -311,8 +253,9 @@ def backtest_strategy(df, symbol, interval, tp_percent=5, sl_percent=1):
 
     return pd.DataFrame(trades)
 
-
 # Save results to CSV
+
+
 def save_to_csv(results, filename='binance_rsi_tp_sl_trading_results.csv'):
     if os.path.exists(filename):
         # Load existing data
@@ -321,9 +264,8 @@ def save_to_csv(results, filename='binance_rsi_tp_sl_trading_results.csv'):
         updated_data = pd.concat([existing_data, results], ignore_index=True)
         # Remove duplicates based on 'Entry Date' and 'Symbol'
         # updated_data = updated_data.drop_duplicates(subset=['Entry Date', 'Symbol'], keep='last')
-        # Save updated data back to CSV
         updated_data.to_csv(filename, index=False)
-        print(f"Results updated in existing file: {filename}")
+        # print(f"Results updated in existing file: {filename}")
     else:
         # Create new file
         results.to_csv(filename, index=False)
@@ -338,7 +280,7 @@ def main():
     # symbol = 'LTC/USDT'  # Trading pair
     # interval = '15m'  # Timeframe (1 hour)
 
-    start_date = '2020-08-01T00:00:00Z'  # Start date in UTC
+    start_date = '2020-01-01T00:00:00Z'  # Start date in UTC
     end_date = '2025-03-01T00:00:00Z'  # End date in UTC
     # end_date = '2025-03-01T00:00:00Z'  # End date in UTC
 
@@ -358,7 +300,7 @@ def main():
         for interval in intervals:
 
             # Fetch data from Binance
-            print(f"Fetching data from Binance for {interval}...")
+            print(f"Fetching data from Binance for {symbol}-{interval}...")
             data = fetch_binance_data(symbol, interval, start_date, end_date)
 
             if data.empty:
