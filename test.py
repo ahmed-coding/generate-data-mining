ad4ffa92df2a02913c6f026ea16c28a658f469df
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2025-03-20 01:13:36 +0300

    add test script for testing

---
+++ test.py
@@ -0,0 +1,72 @@
+intervals = [
+    '1h',
+    '30m',
+    '15m',
+    '5m',
+    '3m',
+    '1m',
+
+]
+
+# for i in intervals:
+#     print(i)
+
+
+symbols = [
+    'BTC/USDT',
+    'ETH/USDT',
+    'BNB/USDT',
+    'SOL/USDT',
+    'SUI/USDT',
+    'ADA/USDT',
+    'XRP/USDT',
+    'DOT/USDT',
+    'LINK/USDT',
+    'LTC/USDT',
+    # 'BCH/USDT',
+    # 'XLM/USDT',
+    # 'AVAX/USDT',
+    # 'UNI/USDT',
+    # 'AAVE/USDT',
+    # 'XTZ/USDT',
+    # 'ATOM/USDT',
+    # 'VET/USDT',
+    # 'ALGO/USDT',
+    # 'HBAR/USDT',
+    # 'MKR/USDT',
+    # 'SNX/USDT',
+    # 'YFI/USDT',
+    # 'COMP/USDT',
+    # 'FTM/USDT',
+    # 'LUNA/USDT',
+    # 'MATIC/USDT',
+    # 'FIL/USDT',
+    # 'CHZ/USDT',
+    # 'THETA/USDT',
+    # 'LRC/USDT',
+    # 'ONE/USDT',
+    # 'SHIB/USDT',
+    # 'QNT/USDT',
+    # 'ENJ/USDT',
+    # 'FLOW/USDT',
+    # 'CAKE/USDT',
+    # 'CELO/USDT',
+    # 'BAT/USDT',
+    # 'REN/USDT',
+    # 'LDO/USDT',
+    # 'RNDR/USDT',
+    # 'XDC/USDT',
+    # 'SUSHI/USDT',
+    # 'BNT/USDT',
+    # 'ZEC/USDT',
+    # 'DCR/USDT',
+    # 'DGB/USDT',
+    # 'NEXO/USDT',
+    # 'RVN/USDT',
+    # 'KSM/USDT',
+]
+
+data = 'BAT/USDT'
+
+print(data.replace('/', ''))
+print(data)
