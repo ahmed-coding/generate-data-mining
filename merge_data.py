7a06690c94b2e2caece387f80da3395cb26d88f3
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2025-03-22 00:14:17 +0300

    Update merge_data.py

--- merge_data.py
+++ merge_data.py
@@ -7,4 +7,23 @@ files = glob.glob("*/*.csv")
 df_list = [pd.read_csv(file) for file in files]
 merged_df = pd.concat(df_list, ignore_index=True)
 
-merged_df.to_csv("merged_file.csv", index=False)
+merged_df.dropna()
+merged_df.to_csv("1merged_file.csv", index=False)
+
+symbols = [
+    'BTC/USDT',
+    'ETH/USDT',
+    'BNB/USDT',
+    'SOL/USDT',
+    'SUI/USDT',
+    'ADA/USDT',
+    'XRP/USDT',
+    'DOT/USDT',
+    'LINK/USDT',
+    'LTC/USDT',
+    'AAVA/USDT',
+    'UNI/USDT',
+    'ACAX/USDT',
+    'BCH/USDT',
+    'XLM/USDT',
+]
